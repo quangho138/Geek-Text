ffa36f3af0ef196aa3fddc464decd97ea2da8ada
asurared19 <asurared19@gmail.com>
2026-02-14 16:34:52 -0500

    Update views.py

--- books/views.py
+++ books/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+//from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+from .models import Author, Book
+from .serializers import AuthorSerializer, BookSerializer
+
+
+class AuthorCreateView(generics.CreateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookByISBNView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = "isbn"
+
+
+class BooksByAuthorView(generics.ListAPIView):
+    serializer_class = BookSerializer
+
+    def get_queryset(self):
+        author_id = self.kwargs["author_id"]
+        return Book.objects.filter(author_id=author_id)
+
