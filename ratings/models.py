e383d7b9cee6d5430035c498f5d422da604763cb
kyle <kylegutierrez51@gmail.com>
2026-02-16 02:59:57 -0500

    Add models for reviews and comments

--- ratings/models.py
+++ ratings/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from books.models import Book
+from users.models import Profile
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Review(models.Model):
+  pk = models.CompositePrimaryKey("user_id", "book_id")
+  user= models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='reviews', null=False)
+  book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reviews', null=False)
+  rating = models.IntegerField(
+    validators=[
+      MinValueValidator(1),
+      MaxValueValidator(5)
+    ]
+  )
+
+  comment = models.CharField(max_length=255, blank=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+
+  class Meta:
+    constraints = [
+        models.CheckConstraint(
+            condition=models.Q(rating__gte=1, rating__lte=5),
+            name='check_rating_between_1_and_5'
+        )
+    ]
