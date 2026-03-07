f33a5ca8d20d4ac5ede218dba345075d1be66906
asurared19 <asurared19@gmail.com>
2026-03-07 18:24:51 -0500

    Update views.py

--- books/views.py
+++ books/views.py
@@ -1,10 +1,34 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from .models import Book
-from .serializers import BookSerializer
+from rest_framework import generics
+from .models import Author, Book
+from .serializers import AuthorSerializer, BookSerializer
 
-@api_view(["GET"])
-def book_list(request):
-    books = Book.objects.all()
-    serializer = BookSerializer(books, many=True)
-    return Response(serializer.data)
+
+class AuthorCreateView(generics.CreateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookByISBNView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = "isbn"
+
+
+class BooksByAuthorView(generics.ListAPIView):
+    serializer_class = BookSerializer
+
+    def get_queryset(self):
+        author_id = self.kwargs["author_id"]
+        return Book.objects.filter(author_id=author_id)
+
+
+class TopSellersView(generics.ListAPIView):
+    serializer_class = BookSerializer
+
+    def get_queryset(self):
+        return Book.objects.order_by("-copies_sold")[:5]
