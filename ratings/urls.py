722877edc68dde0af22522c48c1edefdbecbce8d
kyle <kylegutierrez51@gmail.com>
2026-04-05 15:45:10 -0400

    Fix ratings/urls.py

--- ratings/urls.py
+++ ratings/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from .views import get_ratings
-
-urlpatterns = [
-    path('ratings/', get_ratings, name='get_ratings'),
 from . import views
 
 urlpatterns = [
   path('books/<int:book_id>/reviews/', views.create_review, name="create_review"),
-  path('books/<int:book_id>/reviews/<int:review_id>', views.update_review, name="update_review")
+  path('books/<int:book_id>/reviews/<int:review_id>', views.get_update_review, name="get_update_review"),
+  path('books/<int:book_id>/reviews/avg', views.get_avg_rating, name="get_avg_rating"),
+  path('ratings/', views.get_ratings, name='get_ratings')
 ]
