babed3fe7e544537608bdf870d93166836032398
kyle <kylegutierrez51@gmail.com>
2026-04-05 15:44:22 -0400

    Fix ratings/serializers.py

--- ratings/serializers.py
+++ ratings/serializers.py
@@ -1,13 +1,7 @@
 from rest_framework import serializers
-from .models import Rating
-
-class RatingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Rating
-        fields = "__all__"
 from .models import Review
 
-class ReviewSerializer(serializers.ModelSerializer):
+class RatingSerializer(serializers.ModelSerializer):
       
   # make the comment field optional
   comment = serializers.CharField(required=False, allow_blank=True, max_length=255)
@@ -18,4 +12,5 @@ class ReviewSerializer(serializers.ModelSerializer):
     read_only_fields = ['id', 'book', 'created_at', 'updated_at']
 
     # read_only means the serializer skips "book", "created_at", and "updated_at" during validation, so the user doesn't need to include it in the POST body. It also includes the book in the serialied output (the Response object)
+
 
