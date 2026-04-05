0143f663f0e0696182743547c1c4f5e66cb6da1f
kyle <kylegutierrez51@gmail.com>
2026-04-05 15:43:32 -0400

    Fix models.py file

--- ratings/models.py
+++ ratings/models.py
@@ -1,20 +1,10 @@
 from django.db import models
 from books.models import Book
-
-class Rating(models.Model):
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="ratings")
-    reviewer_name = models.CharField(max_length=100)
-    rating = models.IntegerField()
-    comment = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.reviewer_name} - {self.book.name}"
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 
-class Review(models.Model):
+class Rating(models.Model):
   user= models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews', null=False)
   book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reviews', null=False)
   rating = models.IntegerField(
@@ -53,4 +43,6 @@ with connection.cursor() as cursor:
 
 sets the auto_incrementing value of the id back to 0
 """
+
+
 
