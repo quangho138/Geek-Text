50756fccc7aaf9219a3a9f0de38662a6c22b34f6
kyle <kylegutierrez51@gmail.com>
2026-04-05 15:46:38 -0400

    Fix ratings/admin.py

--- ratings/admin.py
+++ ratings/admin.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
 from .models import Rating
 
-admin.site.register(Rating)
-from .models import Review
-
-class ReviewAdmin(admin.ModelAdmin):
+class RatingAdmin(admin.ModelAdmin):
     readonly_fields = ['created_at', 'updated_at']
 
-admin.site.register(Review, ReviewAdmin)
+admin.site.register(Rating, RatingAdmin)
 
 # since auto_now and auto_now_add fields are non-editable, Django admin hides them by default. So use readonly_fields to display them
