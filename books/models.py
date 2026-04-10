e595ce901d17899d5a6ebe0d88d41bc5b5c0115e
asurared19 <asurared19@gmail.com>
2026-04-09 20:47:53 -0400

    Update models.py

--- books/models.py
+++ books/models.py
@@ -12,16 +12,16 @@ class Author(models.Model):
 
 
 class Book(models.Model):
-    isbn = models.CharField(max_length = 20, unique = True)
-    name = models.CharField(max_length = 200)
+    isbn = models.CharField(max_length = 13, unique = True)
+    name = models.CharField(max_length = 100)
     description = models.TextField()
     price = models.DecimalField(max_digits = 8, decimal_places = 2)
     genre = models.CharField(max_length  = 100)
     publisher = models.CharField(max_length = 150)
     year_published = models.IntegerField()
     copies_sold = models.IntegerField()
-    rating = models.FloatField(default=0.0)  # ✅ ADDED
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+    rating = models.FloatField(default = 0.0)  # ✅ ADDED
+    author = models.ForeignKey(Author, on_delete = models.CASCADE, related_name = "books")
 
     def __str__(self):
         return self.name
