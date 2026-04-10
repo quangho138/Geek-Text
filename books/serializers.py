a351611805c8fddf46bf8eb1d56fc74d022f6cd2
asurared19 <asurared19@gmail.com>
2026-04-09 20:14:36 -0400

    Update serializers.py

--- books/serializers.py
+++ books/serializers.py
@@ -1,6 +1,10 @@
 from rest_framework import serializers
-from .models import Book
+from .models import Author, Book
 
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = "__all__"
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
